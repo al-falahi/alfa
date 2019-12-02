e2dc9da29118bbf00d4e9b15ea7637982b0e077e
Al-Falahi <al-falahi@outlook.com>
2019-12-02 16:17:01 +0100

    Remove padding from header and footer

--- template-parts/footer/content-footer-top.php
+++ template-parts/footer/content-footer-top.php
@@ -1,4 +1,4 @@
-<div class="footer-top py-3">
+<div class="footer-top">
     <div class="container d-flex flex-between flex-center">
         <?php if (is_active_sidebar('footer-top__left')){dynamic_sidebar('footer-top__left');};?>
         <?php if (is_active_sidebar('footer-top__center')){dynamic_sidebar('footer-top__center');};?>
--- template-parts/header/content-header-top.php
+++ template-parts/header/content-header-top.php
@@ -1,4 +1,4 @@
-<div class="header-top py-1">
+<div class="header-top">
     <div class="container d-flex flex-between flex-center">
         <?php if (is_active_sidebar('header_top_left')){dynamic_sidebar('header_top_left');}?>
         <?php if (is_active_sidebar('header_top_center')){dynamic_sidebar('header_top_center');}?>
